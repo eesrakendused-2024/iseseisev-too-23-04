91df7f57942477f1cba751d96419005c1dcf75ce
Liisi Loomets <146342702+LLoomets@users.noreply.github.com>
2024-04-25 14:12:59 +0300

    galerii ja video alustatud

--- fullpage.js
+++ fullpage.js
@@ -1,27 +1,10 @@
 new fullpage('#fullpage', {
 	autoscrolling: true,
 	navigation: true,
-	anchors:['section1', 'section2', 'section3', 'section4','section5','section6'],
+	anchors:['section1', 'section2', 'section3', 'section4','section5','section6', 'section7'],
 	navigationTooltips: ['Esileht', 'Oskused', 'Tööd', 'Hobid', 'Lemmikmuusika', 'Mäng'],
 	showActiveTooltip: true,
 	controlArrows: false,
 	slidesNavigation: true
 })
 
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-    lazyLoading: true,
-    verticalCentered: true,
-		controlArrows: false,
-		slidesNavigation: true,
-    scrollBar: true,
-    navigation: true,
-    scrollOverflow: true,
-    responsiveWidth: 768,
-    // paddingTop: '50px',
-    // fixedElements: '.navbar-fixed-top',
-    menu: '#myNavbar',
-    normalScrollElements: '.modal'
-  });
-  
-});
