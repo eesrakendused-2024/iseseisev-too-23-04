9087f2f7338fe6052ff19111e5f6203cf6ab9cbb
Jan Markus Kähara <146342688+JanMarkusK@users.noreply.github.com>
2024-04-27 20:36:04 +0300

    Leht ise, ilma laienduseta

---
+++ fullPageScript.js
@@ -0,0 +1,9 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+    navigationTooltips: ['My skills','Contact','Works','Hobbies', 'Music', 'Game'],
+    showActiveTooltip: true,
+    controlArrows: false,
+    slidesNavigation: true,
+});
