6259964435cdc689dbe274dc11178b0026c81d46
Sa1tama <159195533+Sa1tamaMan@users.noreply.github.com>
2024-04-28 00:51:42 +0300

    Kodutoo progress (70% done)
    Peab veel lisama library'd ja tegema lõpuni kõik lehed mis jäid.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 new fullpage('#fullpage', {
+  sectionsColor: ['white', 'lightskyblue', 'lightsalmon', 'lightcoral', 'lightsteelblue', 'lightpink', 'lightyellow', 'lightgreen'],
   anchors: ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'],
   navigation: false,
   menu: "#nav",
-  css3: true
+  css3: true,
+  credits: { enabled: true, label: 'Made with fullPage.js', position: 'right'},
+  afterLoad: function(origin, destination, direction) {
+    document.querySelectorAll('#navbar li a').forEach(function(link) {
+      link.classList.remove('selected');
+    });
+
+    var activeLink = document.querySelector('a[href="#' + destination.anchor + '"]');
+    if (activeLink) {
+      activeLink.classList.add('selected');
+    }
+  },
+  afterRender: function() {
+    var s2Slides = document.querySelectorAll('.section[data-anchor="s2"] .slide');
+    for (var i = 0; i < s2Slides.length; i++) {
+      s2Slides[i].setAttribute('data-anchor', 's2-slide' + (i + 1));
+    }
+  }
 });
