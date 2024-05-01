a8a100a37bf8528f4baf94539bf695921fd2b92a
maitrungi <152967590+maitrungi@users.noreply.github.com>
2024-05-01 22:55:17 +0300

    Add files via upload

---
+++ iseseisev.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', 'green', '#C0C0C0', '#ADD8E6'],
+    autoScrolling: true,
+    navigation: true,
+    navigationPosition: 'right',
+    lockAnchors: false,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+    menu: '#menu',
+    navigationTooltips: ['Minu oskused', 'Kontaktandmed', 'Kodutööd', 'Spotify list', 'Hobbies', 'Game'],
+    // Get your license at https://alvarotrigo.com/fullPage/pricing/
+    licenseKey: 'YOUR LICENSE KEY HERE '
+});
